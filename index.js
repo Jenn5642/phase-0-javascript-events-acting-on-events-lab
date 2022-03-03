f0b0ddab45ec6488de831f00a59c9d23dd29e7ee
Jenn Robbins <Jennrobbins3@gmail.com>
2022-03-03 10:48:27 -0500

    commit tree 564fa59ddd8d25b3521718c9bc8de2e976c9732e

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const dodger = document.getElementById("dodger");
 
-
 function moveDodgerLeft() {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
@@ -16,18 +15,16 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-
 function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-  const left = parseInt(leftNumbers, 10);
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const right = parseInt(rightNumbers, 10);
 
-  if (left < 360) {
-    dodger.style.left = `${left + 1}px`;
+  if (right > 0) {
+    dodger.style.left = `${right + 1}px`;
   }
 }
-
-document.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowRight") {
-    moveDodgerRight();
-  }
+document.addEventListener('keydown', function (e) {
+if (e.key === 'ArrowRight') {
+  moveDodgerRight();
+}
 });
